69aad544089ef77d4738b66d1304ebf10f78368e
Jeff Roush <jeff@edfinity.com>
2023-07-29 17:27:29 -0700

    Update fft-1.py

--- fft-1.py
+++ fft-1.py
@@ -11,22 +11,24 @@ zs = arr[:,2]
 fig, ax = plt.subplots()
 ax.set_xlabel('Frequency (Hz)')
 ax.set_ylabel('Amplitude')
+sampling_frequency = 1.0 / 50.0
 
 def plot_fft(arr, label):
   fft = np.fft.fft(arr)
   power_spectrum = np.abs(fft) ** 2
   # power_spectrum = np.abs(fft)
-  freqs = np.fft.fftfreq(len(arr), 0.05).astype(float)
+  freqs = np.fft.fftfreq(len(arr), sampling_frequency).astype(float)
   
   power_spectrum[0] = 0
   power_spectrum[1] = 0
   power_spectrum[2] = 0
 
-  ax.plot(freqs[:len(freqs)//2+1], power_spectrum[:len(freqs)//2+1], label=label)
+  ax.plot(freqs[:len(freqs)//2+1], power_spectrum[:len(freqs)//2+1])
+  # ax.plot(freqs[:len(freqs)//2+1], power_spectrum[:len(freqs)//2+1], label=label)
 
 # plot_fft(zs, label='Z')
 # plot_fft(xs, label='X')
 plot_fft(ys, label='Y')
-ax.legend()
+# ax.legend()
 plt.show()
 
