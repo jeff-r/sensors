9e4af08979933a7c8bada51725b1c4f2b7266cba
Jeff Roush <jeff@edfinity.com>
2023-07-29 17:57:13 -0700

    And it moves

--- thread.py
+++ thread.py
@@ -4,11 +4,19 @@ import threading
 import numpy as np
 import read_csv_data as rcd
 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
 ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
 print(ser.name)         # check which port was really used
 
+N = 1000
 samples = []
-print('samples:', samples)
+for n in range(N):
+  samples.append([0,0,0])
+
+# print('samples:', samples)
 def add_data(line):
   global samples
   if len(line) == 3:
@@ -24,16 +32,43 @@ def read_data(filename):
 def tick(n):
   print("hi", samples[-10:])
 
+def recent_values():
+  return list(map(val, samples[-N:]))
+
+def val(point):
+  return point[2]
+
+print('recent_values:', recent_values())
+
+# Create a figure and axis object
+fig, ax = plt.subplots()
+
+# Define the x axis
+x = np.arange(0, N, 1)
+
+# Create a line plot
+line, = ax.plot(x, recent_values())
+
+# Define the update function for the animation
+def update(n):
+    # Shift the x and y data
+    vals = recent_values()
+    ax.set_ylim(np.min(vals), np.max(vals))
+    line.set_data(x, recent_values())
+    return line,
+
 def main():
   n = 0
   thread = threading.Thread(target=read_data, args=('data/foo.csv',), daemon=True)
   thread.start()
-  while True:
-    n += 1
-    tick(n)
-    time.sleep(1)
+  # Create the animation object
+  ani = animation.FuncAnimation(fig, update, frames=50, interval=50)
+
+  # Show the plot
+  plt.show()
 
 try:
+  print('hi')
   main()
 except KeyboardInterrupt:
   continue_reading = False
