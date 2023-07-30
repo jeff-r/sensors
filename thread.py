d3e60df08dfaa33e24017229078a0412cadb9f05
Jeff Roush <jeff@edfinity.com>
2023-07-30 09:00:00 -0700

    wip

--- thread.py
+++ thread.py
@@ -1,4 +1,5 @@
 import time
+import math
 import serial
 import threading
 import numpy as np
@@ -11,12 +12,11 @@ import matplotlib.animation as animation
 ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
 print(ser.name)         # check which port was really used
 
-N = 200
+N = 500
 samples = []
 for n in range(N):
   samples.append([0,0,0])
 
-# print('samples:', samples)
 def add_data(line):
   global samples
   if len(line) == 3:
@@ -25,18 +25,20 @@ def add_data(line):
 continue_reading = True
 def read_data(filename):
   while continue_reading:
-    str = ser.readline().decode('Ascii').replace("\r\n", "")
-    line = rcd.transform_line(str)
-    add_data(line)
-
-def tick(n):
-  print("hi", samples[-10:])
+    try:
+      str = ser.readline()
+      str = str.decode('Ascii').replace("\r\n", "")
+      line = rcd.transform_line(str)
+      add_data(line)
+    except AttributeError:
+      print('Saw AttributeError')
 
 def recent_values():
   return list(map(val, samples[-N:]))
 
 def val(point):
   return point[2]
+  # return math.sqrt(point[0]**2 + point[1]**2 + point[2]**2)
 
 sampling_frequency = 1.0 / 50.0
 
@@ -60,16 +62,12 @@ values = do_fft()
 freqs = np.fft.fftfreq(len(2*values), sampling_frequency).astype(float)
 x = freqs # [:len(freqs)//2+1]
 
-print('values:', values)
-
 # Create a figure and axis object
 fig, ax = plt.subplots()
 
 # Define the x axis
 # x = np.arange(0, N, 1)
 
-print('x:', len(x))
-print('values:', len(values))
 # Create a line plot
 line, = ax.plot(x, values)
 
@@ -78,23 +76,38 @@ def update(n):
     # Shift the x and y data
     values = do_fft()
     ax.set_ylim(np.min(values), np.max(values))
+    print(np.max(values))
+    #ax.set_ylim(0, 1)
     line.set_data(x, values)
     return line,
 
 def main():
   n = 0
-  thread = threading.Thread(target=read_data, args=('data/foo.csv',), daemon=True)
-  thread.start()
+  global data_thread
+  data_thread = threading.Thread(target=read_data, args=('data/foo.csv',), daemon=True)
+  data_thread.start()
   # Create the animation object
   ani = animation.FuncAnimation(fig, update, frames=50, interval=50)
+  print('just called FuncAnimation')
 
+  fig.canvas.mpl_connect('close_event', on_close)
   # Show the plot
   plt.show()
 
+def on_close(event):
+  print("\n")
+  f = open('out.csv', 'w')
+  for line in recent_values():
+    f.write(str(line))
+    f.write("\n")
+  f.close
+  continue_reading = False
+  # data_thread.join()
+  #ser.close()
+
 try:
-  print('hi')
   main()
 except KeyboardInterrupt:
   continue_reading = False
   ser.close()
-  print('bye', samples)
+  print(recent_values())
